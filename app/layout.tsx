269d4acf0749cb9a8c80555ff216cffd11ea3604
Luiz Almeida <luizhrqas@gmail.com>
2024-11-11 10:27:15 -0300

    update portfolio home

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`antialiased`}
+        style={{ background: 'black'}}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="relative w-full min-h-screen">
+    <div className="relative w-full">
       <motion.video
-        className="absolute top-0 left-0 w-full min-h-full object-cover"
-        src="/bg.mp4"
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="/bg2.mp4"
         autoPlay
         loop
         muted
@@ -16,10 +17,10 @@ export default function Home() {
         transition={{ duration: 1 }}
       />
       <motion.div
-        className="absolute top-0 left-0 w-full min-h-full opacity-80"
+        className="absolute top-0 left-0 w-full h-full opacity-80"
         style={{
           background:
-            "linear-gradient(0deg, rgba(0, 0, 250, 0.5), rgba(0, 0, 0, 0.9))",
+            "linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9))",
         }}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -30,19 +31,8 @@ export default function Home() {
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, delay: 1 }}
-        className="relative z-10 flex pt-5 items-center gap-4 justify-center"
+        className="container mx-auto relative z-10 flex pt-5 items-center justify-center gap-4"
       >
-        <img src="/logo1.svg" alt="Logo" className="w-32 h-32 mb-4" />
-        
-        <div><h1 className="text-white text-xl tracking-wide font-black uppercase">
-          Luiz Almeida
-        </h1>
-        <h2 className="text-white text-xl tracking-wide font-light uppercase">
-          Front-end Engineer
-        </h2></div>
-      </motion.div>
-
-      <motion.div className="relative z-10 pt-5 flex flex-col md:flex-row items-center justify-center gap-9">
         <motion.div
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
@@ -50,7 +40,7 @@ export default function Home() {
           transition={{ duration: 0.75, delay: 1.5 }}
         >
           <motion.img
-            whileHover={{ rotateX: 10, rotateY: -40, scale: 1.5 }}
+            whileHover={{ rotateX: 10, rotateY: -20, scale: 1.1 }}
             transition={{ type: "spring", stiffness: 200 }}
             style={{ perspective: 1000 }}
             src="/pixel.png"
@@ -58,65 +48,108 @@ export default function Home() {
             className="w-24"
           />
         </motion.div>
+
+        <div>
+          <h1 className="text-white text-xl tracking-wide font-black uppercase">
+            Luiz Almeida
+          </h1>
+          <h2 className="text-white text-xl tracking-wide font-light uppercase">
+            Front-end Engineer
+          </h2>
+        </div>
+      </motion.div>
+
+      <motion.div className="container px-10 py-5 mx-auto relative z-10 pt-5 flex flex-col md:flex-row">
         <motion.div
           initial={{ opacity: 0, y: 70 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 1.6 }}
-          className="text-white max-w-lg px-10"
+          className="text-white"
         >
-          <p className="text-lg">
-            a.k.a.{" "}
-            <motion.a
-              whileHover={{ scale: 1.05, fontWeight: "bold" }}
-              transition={{ type: "spring", stiffness: 200 }}
-              href="https://github.com/lhas-dev"
-              className="underline inline-block"
-              target="_blank"
-            >
-              @lhas-dev
-            </motion.a>
-            . I am a <strong>Senior Front-end Engineer</strong> with 14 years of
-            experience in web development.
-            <br />
-            <br />I (usually) build softwares using{" "}
-            <motion.a
-              whileHover={{ scale: 1.05, fontWeight: "bold" }}
-              transition={{ type: "spring", stiffness: 200 }}
-              href="https://github.com/lhas-dev"
-              className="underline inline-block"
-              target="_blank"
-            >
-              React
-            </motion.a>{" "}
-            and{" "}
-            <motion.a
-              whileHover={{ scale: 1.05, fontWeight: "bold" }}
-              transition={{ type: "spring", stiffness: 200 }}
-              href="https://github.com/lhas-dev"
-              className="underline inline-block"
-              target="_blank"
-            >
-              Ruby on Rails
-            </motion.a>
-            .<br />
-            <br />I have a 2-year-old daughter named Rubi. I also love
-            practicing jiu-jitsu (purple belt), growing plants or having a
-            quality time with my family when I&apos;m not programming.
-            <br />
-            <br />I started on programming when I decided to create one custom
-            {' '}<motion.a
-              whileHover={{ scale: 1.05, fontWeight: "bold" }}
-              transition={{ type: "spring", stiffness: 200 }}
-              href="https://github.com/opentibia/server"
-              className="underline inline-block"
-              target="_blank"
-            >
-              Open Tibia Server
-            </motion.a> for me and my friends (like any teenage RPG fan would). <br /><br />After customizing some Lua and C++ scripts as a good <em>script kiddie</em>, I decided to start creating websites.<br /><br />
-            Since then, I worked with some cool startups and corporations like like DI.FM, Renner SA, Pepsi, mLabs and others.
-            <br /><br />I really enjoy shipping softwares to the internet, where a million people will use it. It is very satisfacting for me as developer see some software I helped being used by some random people.<br /><br />I love to learn new things and I&apos;m always looking for new
-            challenges.
+          <h1 className="text-white text-xl tracking-wide font-black uppercase">
+            About Me
+          </h1>
+          <p className="text-lg text-justify mt-4">
+            I&apos;m a Senior Front-End Engineer with 14 years of experience in
+            web development, specializing in building high-quality software
+            using React and Ruby on Rails.
           </p>
+          <p className="text-lg text-justify mt-4">
+            Outside of coding, I&apos;m a dedicated father to my two-year-old
+            daughter, Rubi, and a passionate practitioner of jiu-jitsu, holding
+            a purple belt. When I&apos;m not immersed in technology, I enjoy
+            gardening and spending meaningful time with my family.
+          </p>
+          <h1 className="text-white text-xl tracking-wide font-black uppercase mt-6">
+            My Journey
+          </h1>
+          <p className="text-lg text-justify mt-4">
+            My programming journey began with a passion project: creating a
+            custom Open Tibia Server for my friends and me, driven by a love for
+            RPGs. I dove into scripting with Lua and C++, which sparked my
+            interest in building websites and eventually led me into web
+            development.
+          </p>
+          <h1 className="text-white text-xl tracking-wide font-black uppercase mt-6">
+            About My Career
+          </h1>
+          <p className="text-lg text-justify mt-4">
+            Over the years, I&apos;ve had the opportunity to work with a mix of
+            innovative startups and large corporations, contributing to projects
+            for companies like DI.FM, Renner SA, Pepsi, and mLabs.
+          </p>
+          <p className="text-lg text-justify mt-4">
+            In 2018, I joined a crypto startup during the Bitcoin boom, where I
+            developed solutions using web3.js to integrate the blockchain
+            technology into the web.
+          </p>
+          <p className="text-lg text-justify mt-4">
+            Currently, I&apos;m working on an AI project called cultiva.ai,
+            which empowers Cannabis patients to set up their own grow systems
+            with personalized guidance and resources.
+          </p>
+          <p className="text-lg text-justify mt-4">
+            I&apos;m passionate about delivering software to the web, knowing
+            that it will be used by millions worldwide. There&apos;s nothing
+            more rewarding as a developer than witnessing my work making a
+            difference for all the people around the world.
+          </p>
+          <p className="text-lg text-justify mt-4">
+            I&apos;m a lifelong learner, always on the lookout for new
+            challenges and opportunities to expand my skills.
+          </p>
+          <h1 className="text-white text-xl tracking-wide font-black uppercase mt-6">
+            Contact Me
+          </h1>
+          <div className="flex gap-6 mt-4 mb-4">
+            <motion.a
+              href="https://github.com/lhas-dev"
+              target="_blank"
+              className="text-white hover:text-gray-400 text-2xl"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400 }}
+            >
+              <FaGithub />
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/luizhrqas/"
+              target="_blank"
+              className="text-white hover:text-blue-400 text-2xl"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400 }}
+            >
+              <FaLinkedin />
+            </motion.a>
+            <motion.a
+              href="https://www.instagram.com/lhas.js/"
+              target="_blank"
+              className="text-white hover:text-pink-400 text-2xl"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400 }}
+            >
+              <FaInstagram />
+            </motion.a>
+          </div>
         </motion.div>
       </motion.div>
     </div>
