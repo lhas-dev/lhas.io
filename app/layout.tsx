a0d561ebaceeeee40adbcb1edb778e67dff83714
Luiz Almeida <luizhrqas@gmail.com>
2025-03-01 11:08:21 -0300

    update portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -32,9 +33,19 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased bg-black`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
-import SplitText from "../components/SplitText";
+import { motion } from "framer-motion";
 import Waves from "@/components/Waves";
 import {
   FaDownload,
@@ -54,31 +54,171 @@ export default function Home() {
       link: "https://rovrstack.ai",
     },
   ];
-  const listVariants = {
-    hidden: { opacity: 0, y: -20 },
-    visible: { opacity: 1, y: 0 },
-  };
-
-  const containerVariants = {
-    initial: { opacity: 0 },
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.5, // Animates children one after the other
-      },
-    },
-  };
-
   return (
-    <div className="min-h-screen">
-      <div className="container flex flex-col xl:flex-row gap-20 lg:gap-10 justify-center lg:justify-between mx-auto py-10 lg:py-20 lg:px-10 xl:items-start">
+    <>
+      <motion.header
+        initial={{ opacity: 0, paddingTop: "0px", paddingBottom: "0px" }}
+        animate={{ opacity: 1, paddingTop: "120px", paddingBottom: "120px" }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="bg-black px-10 relative"
+      >
+        <div className="absolute inset-0 w-full h-[140px]">
+          <video
+            src="/video.mp4"
+            autoPlay={true}
+            muted={true}
+            loop={true}
+            className="w-full h-full object-cover filter opacity-50"
+          ></video>
+          <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent pointer-events-none"></div>
+        </div>
+        <div className="container mx-auto relative z-30">
+          <motion.h1
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1.4 }}
+            className="text-5xl font-bold text-white uppercase drop-shadow-md text-center mt-4"
+          >
+            Luiz Almeida
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0, y: -40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1.8 }}
+            className="text-2xl font-light text-white drop-shadow-md uppercase tracking-widest text-center mt-1"
+          >
+            Front-End Engineer
+          </motion.h2>
+        </div>
+      </motion.header>
+      <main className="relative z-20">
+        <div className="container items-center justify-center flex mx-auto">
+          <div className="flex flex-col">
+            <motion.div
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 2.2 }}
+              className="w-[300px] h-[300px] mx-auto rounded-full"
+              style={{
+                backgroundImage: "url('/me3.jpg')",
+                backgroundSize: "108%",
+                backgroundPosition: "top center",
+              }}
+            ></motion.div>
+          </div>
+          <div className="flex flex-col">
+            <motion.p
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 2.5 }}
+              className="text-white font-light max-w-2xl text-left uppercase tracking-widest px-10"
+            >
+              About Me
+            </motion.p>
+            <motion.span
+              initial={{ opacity: 0, width: 0 }}
+              animate={{ opacity: 1, width: "84px" }}
+              transition={{ duration: 1, delay: 3 }}
+              className="bg-white h-[2px] ml-10 my-2"
+            />
+            <motion.h3
+              initial={{ opacity: 0, y: -30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 4.2 }}
+              className="text-white text-4xl leading-tight font-regular max-w-2xl mx-auto px-10"
+            >
+              <strong className="text-purple-500">Olá!</strong> With 14 years of
+              experience in front-end development, I specialize in crafting{" "}
+              <strong className="text-purple-500">pixel-perfect</strong>{" "}
+              applications that scale.
+            </motion.h3>
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 5.8 }}
+              className="text-white text-xl font-light px-10 tracking-wider max-w-2xl mx-auto leading-tight mt-6"
+            >
+              I'm a fast-paced developer from 🇧🇷 Brazil. My focus on user
+              experience has enabled companies of all sizes to achieve their
+              goals.
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 6.2 }}
+              className="text-white text-xl font-light px-10 tracking-wider max-w-2xl mx-auto leading-tight mt-6"
+            >
+              Got an idea? Amazing, let’s make it real.
+              <br />
+              <br />
+              Whether you’re an entrepreneur, a startup founder, or a business
+              looking for a front-end specialist.
+              <br />
+              <br />
+              I’m here to turn concepts into awesome apps. Let’s talk!
+            </motion.p>
+          </div>
+        </div>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 7 }}
+          className="flex mx-auto container items-center justify-center flex-wrap flex-row gap-3 my-20"
+        >
+          <motion.a
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            href="https://github.com/lhas-dev"
+            className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white shadow-md shadow-white/10 hover:shadow-purple-700/10 hover:bg-purple-700 hover:border-purple-700 hover:shadow-lg transition-all duration-300 text-gray-100 px-4 py-2 rounded-full"
+            target="_blank"
+          >
+            <FaGithub className="text-2xl" />
+          </motion.a>
+          <motion.a
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            href="https://www.linkedin.com/in/luizhrqas/"
+            className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white shadow-md shadow-white/10 hover:shadow-purple-700/10 hover:bg-purple-700 hover:border-purple-700 hover:shadow-lg transition-all duration-300 text-gray-100 px-4 py-2 rounded-full"
+            target="_blank"
+          >
+            <FaLinkedin className="text-2xl" />
+          </motion.a>
+          <motion.a
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            href="https://www.instagram.com/lhas.js/"
+            className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white shadow-md shadow-white/10 hover:shadow-purple-700/10 hover:bg-purple-700 hover:border-purple-700 hover:shadow-lg transition-all duration-300 text-gray-100 px-4 py-2 rounded-full"
+            target="_blank"
+          >
+            <FaInstagram className="text-2xl" />
+          </motion.a>
+          <motion.a
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            href="mailto:luizhrqas@gmail.com"
+            className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white shadow-md shadow-white/10 hover:shadow-purple-700/10 hover:bg-purple-700 hover:border-purple-700 hover:shadow-lg transition-all duration-300 text-gray-100 px-4 py-2 rounded-full"
+            target="_blank"
+          >
+            <FaEnvelope className="text-2xl" />
+          </motion.a>
+          <motion.a
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            href="/resume.pdf"
+            target="_blank"
+            className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white shadow-md shadow-white/10 hover:shadow-purple-700/10 hover:bg-purple-700 hover:border-purple-700 hover:shadow-lg transition-all duration-300 text-gray-100 px-4 py-2 rounded-full"
+          >
+            <FaDownload className="text-2xl" /> Resume
+          </motion.a>
+        </motion.div>
+      </main>
+      <div className="absolute z-10 inset-0 w-full h-full opacity-10">
+        <Waves lineColor="purple" backgroundColor="transparent" />
+      </div>
+      {/* <div className="container flex flex-col xl:flex-row gap-20 lg:gap-10 justify-center lg:justify-between mx-auto py-10 lg:py-20 lg:px-10 xl:items-start">
         <div className="flex flex-col md:flex-row gap-10 w-full px-10 lg:px-0 lg:max-w-[600px] lg:mx-auto lg:sticky top-0 lg:pt-10">
-          <motion.img
-            src="/me2.png"
-            alt="Luiz Almeida"
-            className="mx-auto shadow-lg shrink-0 rounded-full w-[160px] h-[160px] object-cover object-center grayscale-[40%] transition-all hover:grayscale-[100%]"
-          />
+          
           <div className="flex flex-col">
             <div className="text-center md:text-left">
               <SplitText
@@ -162,7 +302,7 @@ export default function Home() {
               }}
               href="/resume.pdf"
               target="_blank"
-              className="text-lg font-light tracking-wider flex items-center gap-2 bg-gray-900 text-gray-100 px-4 py-2 rounded-full"
+              className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white hover:bg text-gray-100 px-4 py-2 rounded-full"
             >
               <FaDownload className="text-2xl" /> Resume
             </motion.a>
@@ -172,7 +312,7 @@ export default function Home() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               href="https://github.com/lhas-dev"
-              className="text-lg font-light tracking-wider flex items-center gap-2 bg-gray-900 text-gray-100 px-4 py-2 rounded-full"
+              className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white hover:bg text-gray-100 px-4 py-2 rounded-full"
               target="_blank"
             >
               <FaGithub className="text-2xl" /> Github
@@ -183,7 +323,7 @@ export default function Home() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               href="https://www.linkedin.com/in/luizhrqas/"
-              className="text-lg font-light tracking-wider flex items-center gap-2 bg-gray-900 text-gray-100 px-4 py-2 rounded-full"
+              className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white hover:bg text-gray-100 px-4 py-2 rounded-full"
               target="_blank"
             >
               <FaLinkedin className="text-2xl" /> Linkedin
@@ -194,7 +334,7 @@ export default function Home() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               href="https://www.instagram.com/lhas.js/"
-              className="text-lg font-light tracking-wider flex items-center gap-2 bg-gray-900 text-gray-100 px-4 py-2 rounded-full"
+              className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white hover:bg text-gray-100 px-4 py-2 rounded-full"
               target="_blank"
             >
               <FaInstagram className="text-2xl" /> Instagram
@@ -205,19 +345,19 @@ export default function Home() {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               href="mailto:luizhrqas@gmail.com"
-              className="text-lg font-light tracking-wider flex items-center gap-2 bg-gray-900 text-gray-100 px-4 py-2 rounded-full"
+              className="text-lg font-light tracking-wider flex items-center gap-2 bg-black border border-white hover:bg text-gray-100 px-4 py-2 rounded-full"
               target="_blank"
             >
               <FaEnvelope className="text-2xl" /> Email
             </motion.a>
           </motion.div>
         </div>
-      </div>
-      <div className="w-full h-[300px] relative overflow-hidden opacity-10">
+      </div> */}
+      {/* <div className="w-full h-[300px] relative overflow-hidden opacity-10">
         <Waves />
-      </div>
+      </div> */}
 
-      <div className="py-20 container mx-auto">
+      {/* <div className="py-20 container mx-auto">
         <div className="flex flex-col lg:flex-row gap-10 justify-between">
           <div className="px-10 flex-shrink-0 text-center lg:text-left">
             <SplitText
@@ -323,76 +463,76 @@ export default function Home() {
             </AnimatePresence>
           </motion.div>
         </div>
-      </div>
+      </div> */}
 
-      <footer className="bg-gray-100 py-8 px-10">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row justify-between items-center">
-            <div className="mb-6 md:mb-0">
-              <ul className="flex space-x-6">
-                <li>
-                  <a
-                    href="#about-me"
-                    className="text-gray-600 hover:text-gray-900"
-                  >
-                    About Me
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#technologies"
-                    className="text-gray-600 hover:text-gray-900"
-                  >
-                    Technologies
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#projects"
-                    className="text-gray-600 hover:text-gray-900"
-                  >
-                    Recent Projects
-                  </a>
-                </li>
-              </ul>
+      {/* <footer className="bg-gray-100 py-8 px-10">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+              <div className="mb-6 md:mb-0">
+                <ul className="flex space-x-6">
+                  <li>
+                    <a
+                      href="#about-me"
+                      className="text-gray-600 hover:text-gray-900"
+                    >
+                      About Me
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#technologies"
+                      className="text-gray-600 hover:text-gray-900"
+                    >
+                      Technologies
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#projects"
+                      className="text-gray-600 hover:text-gray-900"
+                    >
+                      Recent Projects
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="flex space-x-4">
+                <a
+                  href="https://github.com/lhas-dev"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
+                  <FaGithub className="text-2xl" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/luizhrqas"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
+                  <FaLinkedin className="text-2xl" />
+                </a>
+                <a
+                  href="mailto:luizhrqas@gmail.com"
+                  className="text-gray-600 hover:text-gray-900"
+                >
+                  <FaEnvelope className="text-2xl" />
+                </a>
+                <a
+                  href="https://www.instagram.com/lhas.js"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
+                  <FaInstagram className="text-2xl" />
+                </a>
+              </div>
             </div>
 
-            <div className="flex space-x-4">
-              <a
-                href="https://github.com/lhas-dev"
-                target="_blank"
-                className="text-gray-600 hover:text-gray-900"
-              >
-                <FaGithub className="text-2xl" />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/luizhrqas"
-                target="_blank"
-                className="text-gray-600 hover:text-gray-900"
-              >
-                <FaLinkedin className="text-2xl" />
-              </a>
-              <a
-                href="mailto:luizhrqas@gmail.com"
-                className="text-gray-600 hover:text-gray-900"
-              >
-                <FaEnvelope className="text-2xl" />
-              </a>
-              <a
-                href="https://www.instagram.com/lhas.js"
-                target="_blank"
-                className="text-gray-600 hover:text-gray-900"
-              >
-                <FaInstagram className="text-2xl" />
-              </a>
+            <div className="mt-6 text-center text-sm text-gray-500">
+              © 2025 Luiz Almeida a.k.a lhas.io. All rights reserved.
             </div>
           </div>
-
-          <div className="mt-6 text-center text-sm text-gray-500">
-            © 2025 Luiz Almeida a.k.a lhas.io. All rights reserved.
-          </div>
-        </div>
-      </footer>
-    </div>
+        </footer> */}
+    </>
   );
 }
